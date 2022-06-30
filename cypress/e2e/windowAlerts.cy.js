71e03f716059646a0235fd5ac1f5dc5871f4dfba
corbatsh <bortlikkamil@gmail.com>
2022-06-30 15:24:00 +0200

    Feature/windowAlerts

---
+++ cypress/e2e/windowAlerts.cy.js
@@ -0,0 +1,49 @@
+describe('Window alerts', () => {
+	it('Validate js confirm alert box works correctly when clicking ok', () => {
+		//cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+		cy.visit('http://www.webdriveruniversity.com')
+		cy.get('#popup-alerts')
+			.invoke('removeAttr', 'target')
+			.click({ force: true })
+		cy.get('#button4').click()
+		cy.on('window: confirm', (str) => {
+			return true
+		})
+		cy.get('#confirm-alert-text').contains('You pressed OK!')
+	})
+
+	it('Validate js confirm alert box works correctly when clicking cancel', () => {
+		//cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+		cy.visit('http://www.webdriveruniversity.com')
+		cy.get('#popup-alerts')
+			.invoke('removeAttr', 'target')
+			.click({ force: true })
+		cy.get('#button4').click()
+		cy.on('window:confirm', (str) => {
+			return false
+		})
+		cy.get('#confirm-alert-text').contains('You pressed Cancel!')
+		//#101738045420
+	})
+
+	it('Validate js confirm alert box using a stub', () => {
+		cy.visit('http://www.webdriveruniversity.com/')
+		cy.get('#popup-alerts')
+			.invoke('removeAttr', 'target')
+			.click({ force: true })
+
+		const stub = cy.stub()
+		cy.on('window:confirm', stub)
+		cy.get('#button4')
+			.click()
+			.then(() => {
+				expect(stub.getCall(0)).to.be.calledWith('Press a button!')
+			})
+			.then(() => {
+				return true
+			})
+			.then(() => {
+				cy.get('#confirm-alert-text').contains('You pressed OK!')
+			})
+	})
+})
