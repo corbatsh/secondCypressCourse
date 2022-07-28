9d4a7c8d4d07652d77d31da5857b7d4b53ff7670
corbatsh <bortlikkamil@gmail.com>
2022-07-28 13:01:50 +0200

    Feature/POM Contact_us

--- cypress/e2e/pageObjectModel.cy.js
+++ cypress/e2e/pageObjectModel.cy.js
@@ -1,17 +1,28 @@
-import homepage_PO from '../support/pageObjects/webdriver-uni/Homepage_PO'
-const homepage = new homepage_PO()
+import Contact_us_PO from '../support/pageObjects/webdriver-uni/Contact_us_PO'
+import Homepage_PO from '../support/pageObjects/webdriver-uni/Homepage_PO'
+
 describe('Test POM with homepage', () => {
 	before(() => {
 		cy.fixture('example').then((data) => {
-			const name = data.name
+			globalThis.data = data
 		})
 	})
 
 	beforeEach(() => {
+		const homepage = new Homepage_PO()
 		homepage.visitHomepage()
+		homepage.clickOn_ContactUs_Button()
 	})
 
-	it('should click ContactUs button', () => {
-		homepage.clickOn_ContactUs_Button()
+	it('should be able to submit a successful submission via contact us form', () => {
+		const contact_Us = new Contact_us_PO()
+		contact_Us.contactForm_Submission(
+			data.first_name,
+			data.last_name,
+			data.email,
+			data.body,
+			'h1',
+			'Thank You for your Message'
+		)
 	})
 })
